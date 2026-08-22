3f98f15c60204fd181cea52ea1afa3f767cf7ce7
venkata1699 <ramana.r1699@gmail.com>
2026-08-22 22:58:31 +0530

    this is the comnit change

--- CalculatorApp/Calculator.cs
+++ CalculatorApp/Calculator.cs
@@ -10,6 +10,11 @@ namespace CalculatorApp
     public class Calculator
     {
         // this is the addition i have done for this calculator
+
+        public double Multiply1(double a, double b)
+        {
+            return a * b;
+        }
         public double Add(double a, double b)
         {
             return a + b;
